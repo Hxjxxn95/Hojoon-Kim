664954a3d99780237ed45ed8dd4720a64ab10b98
Hojoon Kim <99799197+Hxjxxn95@users.noreply.github.com>
2023-05-20 17:54:27 +0900

    [Silver III] Title: 프린터 큐, Time: 312 ms, Memory: 21064 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int time = scanner.nextInt();
+
+        while (time-- > 0) {
+
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+
+            LinkedList<int[]> q = new LinkedList<>();
+
+            for (int i = 0; i < N; i++) {
+                q.offer(new int[] { i, scanner.nextInt() });
+            }
+
+            int count = 0;
+
+            while (!q.isEmpty()) {
+
+                int[] front = q.poll();	
+                boolean isMax = true;	
+
+                
+                for(int i = 0; i < q.size(); i++) {
+
+                    
+                    if(front[1] < q.get(i)[1]) {
+
+                        
+                        q.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            q.offer(q.poll());
+                        }
+                        
+                        isMax = false;
+                        break;
+                    }
+                }
+
+               
+                if(isMax == false) {
+                    continue;
+                }
+                
+                count++;
+                if(front[0] == M) {	
+                    break;
+                }
+
+            }
+
+            sb.append(count).append('\n');
+
+        }
+        System.out.println(sb);
+    }
+
+}
