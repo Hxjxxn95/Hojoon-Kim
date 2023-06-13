dea84cbab675711cd05f9d14fab63d1994ccefe4
Hojoon Kim <99799197+Hxjxxn95@users.noreply.github.com>
2023-06-13 16:54:47 +0900

    [Silver II] Title: 나무 자르기, Time: 528 ms, Memory: 123076 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int K, N;
+    static long[] arr;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    K = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+
+    arr = new long[K];
+
+    long max = 0;
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < K; i++){
+        arr[i] = Integer.parseInt(st.nextToken());
+        max = Math.max(max, arr[i]);
+    }
+
+    System.out.println(binarysearch(1, max+1));
+
+
+    }
+
+    static long binarysearch(long low, long high){
+        long mid = 0;
+        while(low <= high){
+            mid = (low + high) / 2;
+            long temp = 0;
+            for(int i = 0; i < K; i++){
+                if(mid < arr[i])  temp += arr[i] - mid;
+            }
+            if( temp < N) high = mid-1;
+            else low = mid + 1;
+        }
+
+        return (high + low)/2;
+    }
+}
